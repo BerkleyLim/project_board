ca1b30412dcbe5befeb431c1f8861811da412b06
berkleylim <berkleylim16@gmail.com>
2023-07-16 14:25:05 +0900

    feat: Front - API 연동 (CRUD)
    게시판 API 연동 후, 바로 프론트에서 행위 적용

--- board_front/src/App.js
+++ board_front/src/App.js
@@ -11,30 +11,31 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import ModalComponent from "./modal/ModalComponent";
 import update from "immutability-helper";
+import axios from "axios";
 
 function App() {
-  const data = [
-    {
-      bno: 1,
-      title: "제목1",
-      contents: "내용1",
-    },
-    {
-      bno: 2,
-      title: "제목2",
-      contents: "내용2",
-    },
-    {
-      bno: 3,
-      title: "제목3",
-      contents: "내용3",
-    },
-    {
-      bno: 4,
-      title: "제목4",
-      contents: "내용4",
-    },
-  ];
+  // const data = [
+  //   {
+  //     bno: 1,
+  //     title: "제목1",
+  //     contents: "내용1",
+  //   },
+  //   {
+  //     bno: 2,
+  //     title: "제목2",
+  //     contents: "내용2",
+  //   },
+  //   {
+  //     bno: 3,
+  //     title: "제목3",
+  //     contents: "내용3",
+  //   },
+  //   {
+  //     bno: 4,
+  //     title: "제목4",
+  //     contents: "내용4",
+  //   },
+  // ];
   // console.log(data);
 
   // 게시판 데이터 담는 state 값
@@ -62,8 +63,21 @@ function App() {
 
   // html 렌더링 후 데이터 가져오기를 위한 기능
   // [] 는 첫번째만 호출
+  // 이 샘플 데이터를 API로 이용하여 연동
   useEffect(() => {
-    setBoard(data);
+    // 샘플 데이터 연동 제거
+    // setBoard(data);
+
+    // API를 이용하여 조회
+    axios
+      .get("http://localhost:8080/api/board/select/list")
+      .then((res) => {
+        setBoard(res.data);
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
   }, []);
   console.log(board);
 
@@ -77,12 +91,22 @@ function App() {
   // 생성 메서드
   // 이때, 불가변성으로 생성 할 수 있는 기능을 삽입한다.
   const createBoard = (data) => {
-    // front 화면에서 생성하는 것처럼 행동
-    setBoard(
-      update(board, {
-        $push: [data],
+    // 이후 api 호출 후 데이터 삽입
+    axios
+      .post("http://localhost:8080/api/board/insert/board", data)
+      .then((res) => {
+        alert("생성 성공!!");
+        // API 호출 완료 후 front 화면에서 생성 행동
+        setBoard(
+          update(board, {
+            $push: [data],
+          })
+        );
+        console.log(res);
       })
-    );
+      .catch((e) => {
+        console.error(e);
+      });
 
     modalViewToggle(); // 모달창을 닫는다.
   };
@@ -98,23 +122,46 @@ function App() {
 
   // 수정 이벤트
   const updateBoard = (data, index) => {
-    // 불가변성으로 이용하여 해당 데이터 수정
-    setBoard(
-      update(board, {
-        $merge: { [index]: data },
+    // 이후 api 호출 후 데이터 수정
+    axios
+      .post("http://localhost:8080/api/board/update/board", data)
+      .then((res) => {
+        alert("수정 성공!!");
+        // 불가변성으로 이용하여 해당 데이터 수정
+        setBoard(
+          update(board, {
+            $merge: { [index]: data },
+          })
+        );
+        console.log(res);
       })
-    );
+      .catch((e) => {
+        console.error(e);
+      });
     modalViewToggle(); // 모달 창 닫기
     setIsStateChange(!isStateChange); // state 값 변하면 바로 리렌더링하는 기능
   };
 
   // 삭제를 위한 메서드
   const deleteBoard = (index) => {
-    setBoard(
-      update(board, {
-        $splice: [[index, 1]],
+    // 삭제할 데이터 저장후 Parameter로 넘김
+    const deleteData = board[index];
+    // 이후 api 호출 후 데이터 삭제
+    axios
+      .post("http://localhost:8080/api/board/delete/board", deleteData)
+      .then((res) => {
+        alert("삭제 성공!!");
+        console.log(res);
+        // API 호출 이후, 바로 삭제
+        setBoard(
+          update(board, {
+            $splice: [[index, 1]],
+          })
+        );
       })
-    );
+      .catch((e) => {
+        console.error(e);
+      });
   };
 
   return (
